51d06f0a6488d360f9d415d7c77c59429fec9a78
dominikozi s <dominiksandura@gmail.com>
2021-11-05 17:44:57 +0100

    operatory arytmetyczne przypisania itd.

--- php/form_details.php
+++ php/form_details.php
@@ -1,4 +1,6 @@
-
+<?php
+   define("DZIEKI", 'Dzieki za wypelnienie ankiety');
+?>
 
 <!DOCTYPE html>
 
@@ -13,17 +15,22 @@
       </style>
    </head>
    <body>
-   
-      <p>Hi <?php print( $_POST["name"] ); ?>, Dzieki za wypelnienie ankiety</p>
+       <?php 
+         $string0 = 'Czesc, '. $_POST["name"];
+         $string0.= '.<br />';
+         $string0.= DZIEKI;
+         $string0.='<br />'; 
+         echo $string0;
+       ?>
       <p class = "head">Te dane zostały zapisane o tobie:</p>
-      <p>Name: <?php print( $_POST["name"] ); 
+      <p>Name: <?php print( $_POST["name"] . ' '); 
          print( $_POST["surname"] ); ?></p>
-      <p>Email: <?php print( $_POST["month"] ); ?></p>
-      <p>Phone: <?php print( $_POST["email"] ); ?></p>
+      <p>Miesiąc urodzin: <?php print( $_POST["month"] ); ?></p>
+      <p>Email: <?php print( $_POST["email"] ); ?></p>
       <?php
-         $lista_zajetych_emaili = array('dhominico@gmail.com', 'asd@gmail.com', 'jp100@op.pl');
+         $czarna_lista_emaili = array('dhominico@gmail.com', 'asd@gmail.com', 'jp100@op.pl');
          $znaleziono = 0;
-         foreach  ($lista_zajetych_emaili as &$emailtemp){
+         foreach  ($czarna_lista_emaili as &$emailtemp){
             if(strcmp($_POST["email"], $emailtemp)==0){
                $znaleziono++;
             }
@@ -31,7 +38,7 @@
          if($znaleziono==0){
             echo 'Email poprawny. <br />';
          }else{
-            echo 'Email na czarnej liscie!!<br />';
+            echo '<span style="color:#ff0000">Email na czarnej liscie!!</span><br />';
          }
       ?>
       <?php
@@ -44,18 +51,25 @@
             echo 'Twoje samopoczucie pracując u nas byłoby takie samo! <br />';
          }else if($samopoczucie<5 && $samopoczucie>0){
              echo 'Twoje samopoczucie pracując u nas wynosiłoby: '.$samopoczucie*2 .'! <br />';
-             echo 'Już jutro twoje samopoczucie wyniosłoby: ';
-             echo $samopoczucie +1;
-             echo '! <br />';
+             
+             $nowesamopoczucie=$samopoczucie;
+             $nowesamopoczucie+=1;
+             $string1 = 'Już jutro twoje samopoczucie wyniosłoby: ' . $nowesamopoczucie;
+             $string1 .='! <br />';  
+             echo $string1;
              echo 'A nie pracując u nas w niedługim czasie wyniesie: '.$samopoczucie/2 .'! <br />';
              echo 'Decydując sie na niepracowanie u nas już jutro twoje samopoczucie wyniosłoby: ';
              echo $samopoczucie -1;
              echo '! <br />';
          }else {
-            echo 'Twoje samopoczucie pracując u nas wynosiłoby: 9! <br />';
+            $samopoczuciew = &$samopoczucie;
+            $samopoczucie = 9;
+            echo 'Twoje samopoczucie pracując u nas wynosiłoby: ';
+            echo $samopoczuciew;
+            echo '! <br />';
          }
       ?>
-      <p>Phone: <?php print( $_POST["phone"] ); ?></p>
+      <p>Numer telefonu: <?php print( $_POST["phone"] ); ?></p>
       <p class = "head">Naura.</p>   
 
       <?php
@@ -63,7 +77,25 @@
          for ($i = 0; $i < 3; $i++) {
             print( $_POST["name"] );    
          }
+         echo '<br />'
       ?>
-   
+         <?php
+      //whether ip is from share internet
+      if (!empty($_SERVER['HTTP_CLIENT_IP']))   
+      {
+         $ip_address = $_SERVER['HTTP_CLIENT_IP'];
+      }
+      //whether ip is from proxy
+      elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))  
+      {
+         $ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+      }
+      //whether ip is from remote address
+      else
+      {
+         $ip_address = $_SERVER['REMOTE_ADDR'];
+      }
+      echo 'Twoje ip: ' . $ip_address;
+      ?>
    </body>
 </html>
