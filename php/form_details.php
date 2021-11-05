acf9432f8666295d0efa49131f5a15c6aac98cc4
dominikozi s <dominiksandura@gmail.com>
2021-11-05 12:57:05 +0100

    PHP for i foreach etc. (form personal)

--- php/form_details.php
+++ php/form_details.php
@@ -1,3 +1,5 @@
+
+
 <!DOCTYPE html>
 
 <html>
@@ -11,14 +13,36 @@
       </style>
    </head>
    <body>
-      
+   
       <p>Hi <?php print( $_POST["name"] ); ?>, Dzieki za wypelnienie ankiety</p>
       <p class = "head">Te dane zostały zapisane o tobie ruro:</p>
       <p>Name: <?php print( $_POST["name"] ); 
          print( $_POST["surname"] ); ?></p>
       <p>Email: <?php print( $_POST["month"] ); ?></p>
       <p>Phone: <?php print( $_POST["email"] ); ?></p>
+      <?php
+         $lista_zajetych_emaili = array('dhominico@gmail.com', 'asd@gmail.com', 'jp100@op.pl');
+         $znaleziono = 0;
+         foreach  ($lista_zajetych_emaili as &$emailtemp){
+            if(strcmp($_POST["email"], $emailtemp)==0){
+               $znaleziono++;
+            }
+         }
+         if($znaleziono==0){
+            echo 'Email poprawny.';
+         }else{
+            echo 'Email na czarnej liscie!!';
+         }
+      ?>
       <p>Phone: <?php print( $_POST["phone"] ); ?></p>
       <p class = "head">Naura.</p>   
+
+      <?php
+         echo 'Dla sprawdzenia powtórze twoje imie 3 razy. <br />';
+         for ($i = 0; $i < 3; $i++) {
+            print( $_POST["name"] );    
+         }
+      ?>
+   
    </body>
 </html>
