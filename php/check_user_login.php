5cebb71909ca22951c77aabf9e973387558a15c9
Sebastian Sowizdrzal <sowizdrzal.sebastian.kontakt@gmail.com>
2021-11-09 00:12:13 +0100

    base session and full php conversion

---
+++ php/check_user_login.php
@@ -0,0 +1,35 @@
+<?php
+
+$user = array(
+    "sebastian" => "haslo12345",
+    "dominik" => "domi12345"
+);
+
+$uname = $_POST['username'];
+$password = $_POST['password'];
+
+if ($uname != "" && $password != ""){
+
+    $login=0;
+
+    if (array_key_exists($uname, $user)) {
+        if($user[$uname]==$password){
+            $login=1;
+        }
+        else{
+
+        }
+    }
+    else{
+
+    }
+
+    if($login == 1){
+        session_start();
+        $_SESSION['uname'] = $uname;
+        echo 1;
+    }else{
+        echo 0;
+    }
+
+}
---
+++ php/navbar.php
@@ -0,0 +1,12 @@
+           <!--Navigation bar-->
+           <nav class="global-nav">
+                <ul>
+                    <li><a href="/index.html">Home</a></li>
+                    <li><a href="/html/worlds.html">Worlds</a></li>
+                    <li><a href="/html/news.html">News</a></li>
+                    <li><a href="/html/forms.html">Forms</a></li>
+                    <li><a href="/html/guide.html">Guide</a></li>
+                    <li><button id="myBtn"><span>Login</span></button></li>
+                </ul>
+            </nav>
+            <!--end of Navigation bar-->
