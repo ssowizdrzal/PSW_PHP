825de02807340d4a0dc81950de7c691e2cc48c8c
dominikozi <82834358+dominikozi@users.noreply.github.com>
2021-11-22 09:47:36 +0100

    comimity register

--- php/register_user.php
+++ php/register_user.php
@@ -1,4 +1,16 @@
 <?php
+$servername = "localhost";
+$username = "admin";
+$password = "admin";
+$database = "LOLPORTAL";
+
+$conn = new mysqli($servername, $username, $password, $database);
+ 
+ if($conn->connect_errno ) {
+    echo("Connect to database failed");
+    exit();
+ }
+
 
 $uname = $_POST['username'];
 $password = $_POST['password'];
@@ -7,19 +19,24 @@ $surname = $_POST['surname'];
 
 if ($uname != "" && $password != "" && $name != "" && $surname != ""){
 
-    $result_mode;
+    $result = mysqli_query($conn, "SELECT FROM Users (user_login, user_password, user_name, user_surname) WHERE user_login=$uname");
+    $row = mysqli_fetch_row($result);
 
-    $result = mysqli_query($connect, "SELECT FROM Users (user_login, user_password, user_name, user_surname) WHERE user_login=$uname");
+    $count = $row[0];
 
-    if ($result->num_rows) {
-        echo 'Login juz istnieje, modyfikacja !!';
+    if ($count > 0) {
+        echo 1;
         $sql = "UPDATE Users SET user_password='$password' WHERE user_login=$uname";
         $sql = "UPDATE Users SET user_name='$name' WHERE user_login=$uname";
         $sql = "UPDATE Users SET user_surname='$surname' WHERE user_login=$uname";
     }
     else{
-        echo 'Login nie istnieje, rejestracja !!';
-        $sql = "INSERT INTO Users (user_login, user_password, user_name, user_surname)
-        VALUES ($uname, $password , $name, $surname )";      
+        echo 1;
+        $sql = "INSERT INTO Users (user_login, user_password, user_name, user_surname) 
+        VALUES ($uname, $password , $name, $surname )";    
     }
+    $result = mysqli_query($conn,$sql);  
+    
+
+
 }
